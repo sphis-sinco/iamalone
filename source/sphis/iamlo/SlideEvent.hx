ec130f3414641fabed9bb1cb3292cea51713ccf2
Viwu <sinconsistencia@gmail.com>
2025-11-02 19:50:02 -0600

    object related methods

--- source/sphis/iamlo/SlideEvent.hx
+++ source/sphis/iamlo/SlideEvent.hx
@@ -6,11 +6,44 @@ class SlideEvent
 {
 	public var slide_length:Float = 1.0;
 
-	public var objects:Array<FlxBasic> = [];
+	public var objects:Map<String, FlxBasic> = [];
 
 	public function new(slide_length:Float)
 	{
 		this.slide_length = slide_length;
+	}
+
+	public function addObject(id:String, object:FlxBasic)
+	{
+		if (this.objects.exists(id))
+		{
+			trace("Didn't add " + id + " because it already exists");
+			return;
+		}
+
+		this.objects.set(id, object);
+	}
+
+	public function getObject(id:String):FlxBasic
+	{
+		if (!this.objects.exists(id))
+		{
+			return null;
+		}
+		else
+		{
+			return this.objects.get(id);
+		}
+	}
+
+	public function removeObject(id:String)
+	{
+		if (!this.objects.exists(id))
+		{
+			return;
+		}
+
+		this.objects.remove(id);
 	}
 
 	public dynamic function init() {};
