bc91e26ca4532fff2fa3eb3accfb858a07fea15f
Viwu <sinconsistencia@gmail.com>
2025-11-02 19:55:04 -0600

    intro slide event 1

--- source/sphis/iamlo/slides/IntroSlide.hx
+++ source/sphis/iamlo/slides/IntroSlide.hx
@@ -1,8 +1,33 @@
 package sphis.iamlo.slides;
 
-class PlayState extends Slide
+import flixel.FlxSprite;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+class IntroSlide extends Slide
 {
-	override public function new() {
+	override public function new()
+	{
+		var event_1:SlideEvent = new SlideEvent(1.0);
+		event_1.init = function()
+		{
+			var nicom = new FlxSprite();
+			nicom.loadGraphic('assets/images/nicom-front-face.png');
+
+			event_1.addObject('nicom', nicom);
+		};
+
+		event_1.create = function()
+		{
+			var nicom:FlxSprite = cast event_1.getObject('nicom');
+
+			nicom.alpha = 0;
+			nicom.screenCenter();
+
+			FlxTween.tween(nicom, {alpha: 0}, 1);
+		}
+
+		this.events = [event_1];
 		super();
 	}
 }
